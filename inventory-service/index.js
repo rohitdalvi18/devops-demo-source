f6e6574a131fc16667c5455990ab3f450cb4db61
rohitdalvi18 <rohit.dalvi2001@gmail.com>
2025-05-07 13:16:35 -0700

    feat: sync inventory with orders

--- inventory-service/index.js
+++ inventory-service/index.js
@@ -6,12 +6,5 @@ app.get('/inventory', (req,res) => {
   res.json([{ sku:"Apple", qty:100 }, { sku:"Banana", qty:200 }, { sku:"Mango", qty:300 }]);
 });
 
-// app.get('/inventory', (req, res) => {
-//   // Explicitly set HTML content type
-//   res.set('Content-Type', 'text/html');      // [1]
-//   // Send a simple HTML response
-//   res.send('<h1>Inventory List</h1><p>Here are your items…</p>');
-// });
-
 
 app.listen(port, ()=>console.log(`inventory-service on ${port}`));
